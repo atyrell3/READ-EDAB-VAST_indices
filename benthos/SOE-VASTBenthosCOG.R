d7b068b4597158f81e4bcb17d34260563e94bba7
MGrezlik <maxwell.grezlik@noaa.gov>
2025-12-30 10:15:15 -0500

    added SOE R scripts. updated indices

---
+++ benthos/SOE-VASTBenthosCOG.R
@@ -0,0 +1,52 @@
+# format center of gravity output for ecodata submission and SOE
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+SOEinputsCOG <- function(infile, season, taxa, outfile) {
+  
+  cogout <- readRDS(infile)
+  
+  benthoscog <- as.data.frame(cogout$COG_Table) |>
+    dplyr::mutate(direction = ifelse(m==1, "Eastward", "Northward")) |>
+    dplyr::select("Time" = Year,
+                  "Center of Gravity" = COG_hat, 
+                  "Center of Gravity SE" = SE,
+                  direction) |>
+    tidyr::pivot_longer(c("Center of Gravity", "Center of Gravity SE"), 
+                        names_to = "Var", values_to = "Value") |>
+    #direction into Var
+    tidyr::unite(Var, direction:Var, sep = " ") |>
+    dplyr::mutate(Units = "km",
+                  EPU = "ALLEPU") |>
+    dplyr::select(Time, Var, Value, EPU, Units)
+  
+  benthoscog$Var <- stringr::str_c(season, taxa, benthoscog$Var, sep = " ")
+  
+  #readr::write_csv(benthosindex, outfile)
+  saveRDS(benthoscog, outfile)
+  
+  
+}
+
+# make data files
+SOEinputsCOG(infile = "benthos/pyindex/macrobenthos_fall_500_cov/cogout.rds",
+             season = "Fall", 
+             taxa = "Macrobenthos",
+             outfile = "benthos/pyindex/fallmacrobenthoscog.rds")
+
+SOEinputsCOG(infile = "benthos/pyindex/macrobenthos_spring_500_cov/cogout.rds",
+             season = "Spring", 
+             taxa = "Macrobenthos",
+             outfile = "benthos/pyindex/springmacrobenthoscog.rds")
+
+SOEinputsCOG(infile = "benthos/pyindex/megabenthos_fall_500_cov/cogout.rds",
+             season = "Fall", 
+             taxa = "Megabenthos",
+             outfile = "benthos/pyindex/fallmegabenthoscog.rds")
+
+SOEinputsCOG(infile = "benthos/pyindex/megabenthos_spring_500_cov/cogout.rds",
+             season = "Spring", 
+             taxa = "Megabenthos",
+             outfile = "benthos/pyindex/springmegabenthoscog.rds")
---
+++ benthos/SOE-VASTBenthosIndices.R
@@ -0,0 +1,87 @@
+# create rds for ecodata input
+# aim for similar structure to other ecodata datasets
+
+
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+SOEinputs <- function(infile, season, taxa, outfile) {
+  
+  splitoutput <- read.csv(infile)
+  
+  # warning, hardcoded. obviously
+  stratlook <- data.frame(Stratum = c("Stratum_1",
+                                      "Stratum_2",
+                                      "Stratum_3",
+                                      "Stratum_4",
+                                      "Stratum_5"),
+                          Region  = c("AllEPU", 
+                                      "MAB",
+                                      "GB",
+                                      "GOM",
+                                      "SS"))
+  
+  benthosindex <- splitoutput %>%
+    left_join(stratlook) %>%
+    dplyr::select(Time, 
+                  EPU = Region, 
+                  "Biomass Index Estimate" = Estimate, 
+                  "Biomass Index Estimate SE" = Std..Error.for.Estimate) %>%
+    tidyr::pivot_longer(c("Biomass Index Estimate", "Biomass Index Estimate SE"), 
+                        names_to = "Var", values_to = "Value") %>%
+    dplyr::filter(EPU %in% c("MAB", "GB", "GOM", "AllEPU")) %>%
+    dplyr::mutate(Units = "relative grams per stomach") %>%
+    dplyr::select(Time, Var, Value, EPU, Units)
+  
+  benthosindex$Var <- stringr::str_c(season, taxa, benthosindex$Var, sep = " ")
+  
+  saveRDS(benthosindex, outfile)
+  
+} 
+
+
+# make data files
+SOEinputs(infile = "benthos/pyindex/macrobenthos_fall_500_cov/Index.csv",
+          season = "Fall", 
+          taxa = "Macrobenthos",
+          outfile = "benthos/pyindex/fallmacrobenthosindex.rds")
+
+SOEinputs(infile = "benthos/pyindex/macrobenthos_spring_500_cov/Index.csv",
+          season = "Spring", 
+          taxa = "Macrobenthos",
+          outfile = "benthos/pyindex/springmacrobenthosindex.rds")
+
+SOEinputs(infile = "benthos/pyindex/megabenthos_fall_500_cov/Index.csv",
+          season = "Fall", 
+          taxa = "Megabenthos",
+          outfile = "benthos/pyindex/fallmegabenthosindex.rds")
+
+SOEinputs(infile = "benthos/pyindex/megabenthos_spring_500_cov/Index.csv",
+          season = "Spring", 
+          taxa = "Megabenthos",
+          outfile = "benthos/pyindex/springmegabenthosindex.rds")
+
+
+# test plot
+# foragewide <- forageindex %>%
+#   pivot_wider(names_from = Var, values_from = Value)
+# 
+# 
+# ggplot(foragewide, aes(x=Time, y=`Forage Fish Biomass Estimate`, colour = EPU)) +
+#   geom_errorbar(aes(ymin=`Forage Fish Biomass Estimate`+`Forage Fish Biomass Estimate SE`, 
+#                     ymax=`Forage Fish Biomass Estimate`-`Forage Fish Biomass Estimate SE`))+
+#   geom_point()+
+#   geom_line()
+
+macrowide <- fallmacrobenthosindex |> tidyr::pivot_wider(names_from = Var, values_from = Value)
+
+ggplot2::ggplot(macrowide, ggplot2::aes(x=Time, y=`Fall Macrobenthos Biomass Index Estimate`, colour=EPU)) + 
+  ggplot2::geom_ribbon(ggplot2::aes(ymin=`Fall Macrobenthos Biomass Index Estimate`-`Fall Macrobenthos Biomass Index Estimate SE`, 
+                                      ymax=`Fall Macrobenthos Biomass Index Estimate`+`Fall Macrobenthos Biomass Index Estimate SE`,
+                                    fill=EPU),
+                      alpha=0.5)+
+  ggplot2::geom_point()+
+  ggplot2::geom_line() #+
+  #ggplot2::facet_wrap(~EPU)
+
